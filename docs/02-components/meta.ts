7f460c3cc52d9c7da51f44c58837fa4bccd59e30
Allister Antosik <allistera@gmail.com>
2026-09-02 05:33:58 +0000

    docs: align system guide with current architecture

--- docs/02-components/meta.ts
+++ docs/02-components/meta.ts
@@ -3,5 +3,5 @@ import { defineMeta } from 'blume';
 export default defineMeta({
   title: 'Components',
   icon: 'layout-grid',
-  pages: ['cookie-web', 'cookie-worker', 'cookie-ios', 'cookie-docs'],
+  pages: ['cookie-web', 'cookie-worker', 'cookie-ios', 'cookie-docs', 'cookie-agents'],
 });
